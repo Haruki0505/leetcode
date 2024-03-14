9a6276a01909a4939b3731542b5502f407979ca7
Haruki Kasai <lijingyaohui542@gmail.com>
2024-03-14 23:30:24 +0900

    122

---
+++ medium/122_best_time_to_buy_and_sell_stock_II.py
@@ -0,0 +1,19 @@
+nums = [1,2,3,4,5]
+
+def max_profit(prices):
+  min_price = prices[0]
+  max_profit = 0
+
+  for i in range(len(prices)):
+    if prices[i] > min_price:
+      max_profit += prices[i] - min_price
+    min_price = prices[i]
+
+  return max_profit
+
+
+def main():
+  print(max_profit(nums))
+
+if __name__ == '__main__':
+  main()
